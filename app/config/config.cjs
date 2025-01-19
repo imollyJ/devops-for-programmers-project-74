462524f7c2a8ccb20019160718987ed2fefc6ac5
imollyJ <danabramov8@gmail.com>
2025-01-19 11:57:13 -0500

    config update

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -2,11 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: 'postgres',
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+   
   },
   production: {
     dialect: 'postgres',
